5822676bc16a0e56e16e44050a0acdd1a17c43e2
teu <pjasiulewicz@gmail.com>
2015-12-21 14:41:59 +0100

    ammend in apache start file

--- docker/web/apache.sh
+++ docker/web/apache.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-source /etc/apache2/envvars && ./usr/sbin/apache2 -DFOREGROUND >> /var/log/apache2/apache.log 2>&1
+source /etc/apache2/envvars && /usr/sbin/apache2 -DFOREGROUND >> /var/log/apache2/apache.log 2>&1
